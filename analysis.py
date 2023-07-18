60202dc2320763d8e4e91309f7a6f09b2d335b19
LucasSteinach <steinach@mail.ru>
2023-07-19 00:34:56 +0400

    1.2

--- analysis.py
+++ analysis.py
@@ -1,87 +1,60 @@
-# cell 1
+"""
+Input: .csv, output file of main.py
+Output: .csv contains compromised and associated with them records;
+"""
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
-from pprint import pprint
-from sklearn.cluster import SpectralClustering
-from sklearn.preprocessing import OneHotEncoder
-import networkx as nx
-from pandas.plotting import scatter_matrix
 
-# cell 2
+
 fraudulent_device = '91b12379-8098-457f-a2ad-a94d767797c2'
 fraudulent_account = '0007f265568f1abc1da791e852877df2047b3af9'
 data = pd.read_csv('output.csv')
-print(data.columns)
 
 
-# cell 2.5
-def get_node(row: pd.Series, fields_list: list):   # -> tuple:
-    result = tuple(row[fields_list].to_list())
-    return result
+associated_fingerprints = list(set(data['device_fingerprint'][
+    ((data['device_id'] == fraudulent_device)
+    | (data['identity'] == fraudulent_account))
+    & (data['device_fingerprint'] != 'No data')
+].values))
 
 
-fields = ['identity', 'device_id', 'device_fingerprint']
-# print(get_node(data.iloc[2], fields))
-
-# cell 3
-# features = data[['lon', 'lat']]
-#
-# encoded_features = pd.get_dummies(features)
-#
-#
-# spectral = SpectralClustering(n_clusters=3, affinity='nearest_neighbors')
-# labels = spectral.fit_predict(encoded_features)
-# print(set(labels))
-
-# cell 4
-data['color'] = ['red' if list(dict(row).values()).count(fraudulent_account) > 0
-                 or list(dict(row).values()).count(fraudulent_device) > 0
-                 else 'blue'
-                 for (index, row) in data.iterrows()]
-
-data = data.sort_values(by='color')
-# data.plot(kind='scatter', x='lon', y='lat', c=data['color'])
-# plt.title('Cluster Visualization')
-# plt.show()
-
-# cell 5
-pprint(set(data['device_id'][data['color'] == 'red']))
-associated_devices = list(data['device_id'][data['color'] == 'red'])
-print('devices amount =', len(set(data['device_id'][data['color'] == 'red'])))
-
-pprint(set(data['identity'][data['color'] == 'red']))
-associated_accounts = list(data['identity'][(data['color'] == 'red') & (data['identity'] != '-')])
-# print('accounts amount =', len(set(data['identity'][data['color'] == 'red'])))
-# print('===========================================================')
-# print('unique accounts')
-# pprint(set(data['identity'][(data['device_id'].isin(associated_devices)) & (data['identity'] != '-')]))
-# print('===========================================================')
-# print('unique devices')
-# pprint(data[['device_id']][(data['identity'].isin(associated_accounts)) & (data['proxy'] is True)])
-# print('===========================================================')
-# print('unique fingerprints')
-# pprint(set(data['device_fingerprint'][(data['device_id'].isin(associated_devices))
-#                                       & (data['identity'] != '-')
-#                                       & (data['identity'].isin(associated_accounts))]))
-# associated_fingerprints = list(set(data['device_fingerprint'][(data['device_id'].isin(associated_devices))
-#                                                               & (data['identity'] != '-')
-#                                                               & (data['identity'].isin(associated_accounts))]))
-# print('===========================================================')
-# print('accounts with associated fingerprints')
-# pprint(set(data['identity'][(data['device_fingerprint'].isin(associated_fingerprints))]))
+fraudulent_data = data[(data['device_id'] == fraudulent_device)
+                       | (data['identity'] == fraudulent_account)
+                       | (data['device_fingerprint'].isin(associated_fingerprints))
+                       ].copy()
+fraudulent_data.loc[:, 'color'] = 'red'
 
 
-# data.plot(kind='scatter', x='lon', y='lat', c=data['color'])
-# plt.title('Cluster Visualization')
-# plt.show()
+parameters_of_user_agent = {
+    'os': [],
+    'browser': [],
+    'screen': [],
+}
+for index, row in data[data['device_fingerprint'].isin(associated_fingerprints)].iterrows():
+    parameters_of_user_agent['os'].append(row['os'])
+    parameters_of_user_agent['browser'].append(row['browser'])
+    parameters_of_user_agent['screen'].append(row['screen'])
+for key in parameters_of_user_agent.keys():
+    parameters_of_user_agent[key] = list(set(parameters_of_user_agent[key]))
 
-# cell 6
-G = nx.Graph()
-#
-G.add_nodes_from(list(data.apply(lambda x: get_node(x, fields), axis=1, )))
-#
-nx.draw(G, with_labels=False, node_color=list(data['color']))
-# nx.write_graphml(nx.MultiGraph(), 'graph.graphml')
-#
+
+associated_data = data[
+        (data['os'].isin(parameters_of_user_agent['os']))
+        & (data['browser'].isin(parameters_of_user_agent['browser']))
+        & (data['screen'].isin(parameters_of_user_agent['screen']))
+        & (data['country'] == 'Russia')
+        & (data['device_id'] != fraudulent_device)
+        & (data['identity'] != fraudulent_account)
+        & ~(data['device_fingerprint'].isin(associated_fingerprints))
+    ].copy()
+associated_data.loc[:, 'color'] = 'yellow'
+
+result_data = pd.concat([fraudulent_data, associated_data])
+result_data.to_csv('analysis_result.csv')
+
+result_data = result_data.sort_values(by='color')
+result_data.plot(kind='scatter', x='lon', y='lat', c=result_data['color'])
 
 plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# Input data: flat table in test.xlsx
-# Output data: output.csv; contains data from test.xlsx, extended with information about IP
-#                                                                      (source: http://ip-api.com)
-
+"""
+Input data: flat table in test.xlsx
+Output data: output.csv; contains data from test.xlsx,
+extended with information about IP (source: http://ip-api.com)
+"""
 
 import pandas as pd
 from pprint import pprint
@@ -10,15 +11,18 @@ import csv
 import json
 import time
 
-
 initial_data = pd.read_excel('test.xlsx')
 all_ips = list(initial_data['ips'])
 output_name = 'output.csv'
 
 
-# 1. splits ips records by ','
-# 2. creates a list of unique ips from initial_data['ips']
 def get_ip_list(raw_list: list) -> list:
+    """
+    1. splits ips records by ','
+    2. creates a list of unique ips from initial_data['ips']
+    :param raw_list: is a list from column 'ips' in test.xlsx
+    :return: list of unique ips
+    """
     result = list()
     for unit in raw_list:
         if unit.find(',') == -1:
@@ -28,8 +32,12 @@ def get_ip_list(raw_list: list) -> list:
     return list(set(result))
 
 
-# returns dict with information about IP {'ip': dict of parameters}
 def connect_api(ips: list) -> dict:
+    """
+    returns dict with information about IP {'ip': dict of parameters}
+    :param ips: list of unique ips
+    :return: dict of ips, structure: {ip: {values: data} }
+    """
     result = dict()
     for ip in ips:
         response = requests.get(api_data['base_url'] + ip + "?fields=" + ','.join(api_data['fields'].keys()))
@@ -62,6 +70,11 @@ def connect_api(ips: list) -> dict:
 
 
 def create_flat_table(raw_table: pd.DataFrame) -> pd.DataFrame:
+    """
+    adds new rows with single ip, removes rows with merged ips (example: '192.168.0.1,192.168.0.2')
+    :param raw_table: pd.DataFrame with merged ips
+    :return: pd.DataFrame, each row contains single ip
+    """
     row_to_remove = []
     result = pd.DataFrame(dict(raw_table))
     for index, row in raw_table.iterrows():
@@ -72,13 +85,23 @@ def create_flat_table(raw_table: pd.DataFrame) -> pd.DataFrame:
     result = result.drop(row_to_remove).reset_index(drop=True)
     return result
 
-# returns list of ips without "success" status
+
 def get_failed_ip_list(ips: dict) -> list:
+    """
+    returns list of ips without "success" status (errors during connect_api-function)
+    :param ips: dict, result of connect_api-function
+    :return: list of ip which status is not "success"
+    """
     return [key for key, value in ips.items() if value.get('status') != 'success']
 
 
-# for each ip returns list(row) of data to concate with initial_data
 def concate_data(ips: str, fields: list, source: dict) -> list:
+    """
+    :param ips: single value of column 'ips' in initial dataframe
+    :param fields: fields to concate
+    :param source: dict, result of connect_api
+    :return: list(row) of data to concate with initial_data, set '' for cell if there is no value of some column
+    """
     result = list()
     if ips.find(',') == -1:
         for field in fields:
@@ -129,14 +152,8 @@ api_data = {
 }
 
 initial_data = create_flat_table(initial_data)
-print(initial_data)
 
-# УБЕРИ КОММЕНТАРИЙ
-# data_to_concate = connect_api(api_data['ips'])
-
-# УБЕРИ МЕНЕДЖЕР КОНТЕКСТА И ФАЙЛ
-with open("ip_dict.json", 'r') as file:
-    data_to_concate = pd.read_json(file).to_dict()
+data_to_concate = connect_api(api_data['ips'])
 
 # name of columns to concate (fields without 'status', 'message', 'query')
 concate_fields = list(api_data['fields'].keys())[2:-1]
